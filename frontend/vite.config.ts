5aef0d819ecb89c94f67dcfb0c6bffb374ae5a53
Soham Dalvi <sohamdalvi59@gmail.com>
2026-09-10 22:52:56 +0530

    Create vite.config.ts

---
+++ frontend/vite.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  // This must match your exact GitHub repository name!
+  base: '/Zyqor.Intern/',
+})
